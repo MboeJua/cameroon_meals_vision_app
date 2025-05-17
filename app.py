cb883e62384b58b59b628c5701b122d0bda47ae2
mboe jua <paulinusjua@users.noreply.huggingface.co>
2025-05-17 12:48:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from google.cloud import storage, bigquery
 from fastai.vision.all import load_learner, PILImage
 from fastai.vision.augment import Resize  
 from pathlib import Path
-from transformers import pipeline
 
 # Setup GCP credentials
 credentials_content = os.environ['gcp_cam']
